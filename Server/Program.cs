c71667eb1dd68e1f2fe5fb51dee152d0d7e00313
Kseniia Vodolazhska <kseniia@Kseniias-MacBook-Pro.local>
2026-03-08 10:12:18 +0200

    upgrade.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,55 +1,68 @@
+namespace BWS.ZMC.Server;
+
 using System;
 using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Oqtane.Extensions;
 using Oqtane.Infrastructure;
 using Oqtane.Shared;
 
-namespace Oqtane.Application.Server
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        if (builder.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConsole();
+            builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Debug);
 
-            AppDomain.CurrentDomain.SetData(Constants.DataDirectory, Path.Combine(builder.Environment.ContentRootPath, "Data"));
-
-            var configurationBuilder = new ConfigurationBuilder()
-                .SetBasePath(builder.Environment.ContentRootPath)
-                .AddJsonFile("appsettings.json", false, true)
-                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
-                .AddEnvironmentVariables();
-            var configuration = configurationBuilder.Build();
-
-            builder.Services.AddOqtane(configuration, builder.Environment);
-
-            var app = builder.Build();
-
-            var corsService = app.Services.GetRequiredService<ICorsService>();
-            var corsPolicyProvider = app.Services.GetRequiredService<ICorsPolicyProvider>();
-            var syncManager = app.Services.GetRequiredService<ISyncManager>();
-
-            app.UseOqtane(configuration, builder.Environment, corsService, corsPolicyProvider, syncManager);
-
-            var databaseManager = app.Services.GetService<IDatabaseManager>();
-            var install = databaseManager.Install();
-            if (!string.IsNullOrEmpty(install.Message))
+            // important to get blazor errors in console
+            builder.Services.AddServerSideBlazor().AddCircuitOptions(options =>
             {
-                var filelogger = app.Services.GetRequiredService<ILogger<Program>>();
-                if (filelogger != null)
-                {
-                    filelogger.LogError($"[Oqtane.Server.Program.Main] {install.Message}");
-                }
-            }
-            else
+                options.DetailedErrors = true;
+            });
+        }
+
+        AppDomain.CurrentDomain.SetData(Constants.DataDirectory, Path.Combine(builder.Environment.ContentRootPath, "Data"));
+
+        var configurationBuilder = new ConfigurationBuilder()
+            .SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile("appsettings.json", false, true)
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+            .AddEnvironmentVariables();
+        var configuration = configurationBuilder.Build();
+
+        builder.Services.AddOqtane(configuration, builder.Environment); // Adds the core Blazored library services
+
+        var app = builder.Build();
+
+        var corsService = app.Services.GetRequiredService<ICorsService>();
+        var corsPolicyProvider = app.Services.GetRequiredService<ICorsPolicyProvider>();
+        var syncManager = app.Services.GetRequiredService<ISyncManager>();
+
+        app.UseOqtane(configuration, builder.Environment, corsService, corsPolicyProvider, syncManager);
+
+        var databaseManager = app.Services.GetService<IDatabaseManager>();
+        var install = databaseManager.Install();
+        if (!string.IsNullOrEmpty(install.Message))
+        {
+            var filelogger = app.Services.GetRequiredService<ILogger<Program>>();
+            if (filelogger != null)
             {
-                app.Run();
+                filelogger.LogError($"[Oqtane.Server.Program.Main] {install.Message}");
             }
+        }
+        else
+        {
+            app.Run();
         }
     }
 }
